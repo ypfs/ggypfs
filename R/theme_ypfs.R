c02cadd6a627365eb634cc238f6bbd202f8bfd2d
Corey Runkel <39202587+runkelcorey@users.noreply.github.com>
2021-11-03 12:46:51 -0400

    Export theme_ypfs

--- R/theme_ypfs.R
+++ R/theme_ypfs.R
@@ -1,3 +1,4 @@
+#' @export
 theme_ypfs <- function(...) {  theme_classic() +
   suppressWarnings(theme(panel.grid.major.y = element_line(color = "grey92"),
         panel.grid.major.x = element_line(color = "grey92", size = rel(.2)),
